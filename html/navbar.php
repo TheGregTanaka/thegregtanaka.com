614864ea2b5a32ad4f1d88c9a52030a3da68d7d0
Ubuntu <ubuntu@ip-172-31-34-91.us-west-2.compute.internal>
2017-07-11 05:10:08 +0000

    included fontawesome in navbar

--- html/navbar.php
+++ html/navbar.php
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="site.css">
+	<script src="https://use.fontawesome.com/5912553d90.js"></script>
 </head>
 <body>
 
