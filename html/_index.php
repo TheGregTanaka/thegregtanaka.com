0836b2833c67d3c7c7d67d09ad09e4dce7fd7c31
root <root@ip-172-31-34-91.us-west-2.compute.internal>
2017-07-11 03:54:44 +0000

    First commit of site.

---
+++ html/_index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello!</title>
+</head>
+<body>
+<p>Oh my goodness, this is so embarrassing! You're already here and I haven't even put anything on!
+</body>
+</html>";
