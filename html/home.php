608055148878bed69e0328964c8937e73e69fc3a
Ubuntu <ubuntu@ip-172-31-34-91.us-west-2.compute.internal>
2017-07-20 17:49:14 +0000

    old index now home, index is landing page.Added style, fixed formatting.

---
+++ html/home.php
@@ -0,0 +1,29 @@
+<?php
+include('navbar.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="site.css">
+</head>
+<body>
+
+<div class="container">
+
+	<div class="jumbotron">
+		<h1>It's almost a site now!</h1>
+		<p>Look at me!! I've added some super basic bootstrap and css to make it look better.</p> 
+	</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
+
